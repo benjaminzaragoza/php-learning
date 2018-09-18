93de6e63c2080fe75bfba6972dceab0036a7a85f
benjaminzaragoza <benjaminzaragoza@iesebre.com>
2018-09-18 20:38:48 +0200

    Xivatos php amb echo

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?php
+
+//XIVATOS Control + / (teclat numeric)
+
+//echo "hola mon"; //hardcoded toy ejemple
+
+
+//var_dump('Hellow hold');
+//$greeting='hola mon';
+//echo $greeting;
+
+//Test -> tdd
+
+
+
